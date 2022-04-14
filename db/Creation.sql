ef323308ac9c144993d1a14e019def5919fc7e27
Mohamed Kouriat <mohamed.kouriat.etu@univ-lille.fr>
2022-04-14 15:14:21 +0200

    Correction_sql

--- db/Creation.sql
+++ db/Creation.sql
@@ -1,16 +1,16 @@
-create table admin {
-id_admin integer PRIMARY KEY NOT NULL,
+create table admin (
+id_admin integer PRIMARY KEY,
 full_name VARCHAR(100),
 username VARCHAR(100),
 password VARCHAR(100)
 
-}
+)
 
-create table order {
+create table order (
 
-id_order integer PRIMARY KEY NOT NULL,
+id_order integer PRIMARY KEY,
 food VARCHAR(100),
-price VARCHAR(10,2),
+price decimal(10,2),
 quantity integer,
 total VARCHAR(100),
 date VARCHAR(100),
@@ -19,22 +19,20 @@ customor_contact Varchar(100),
 customor_mail Varchar(255),
 customor_address Varchar(255)
 
-}
+)
 
-create table category {
+create table category (
 id_categoty Serial PRIMARY Key,
 title Varchar(100),
-description Varchar(255),
+description Varchar(255)
+)
 
 
-}
-
-
-create table food {
+create table food (
 id_food Serial PRIMARY Key,
 title Varchar(100),
 description Varchar(255),
 id_categoty  Varchar (100),
-image_name VARCHAR(255),
+image_name VARCHAR(255)
 
-}
+)
