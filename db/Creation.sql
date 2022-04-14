8dc4dca3ef51ba7ed0966b4bd672d65a4bab672e
Mohamed Kouriat <mohamed.kouriat.etu@univ-lille.fr>
2022-04-15 00:59:49 +0200

    Creation db

--- db/Creation.sql
+++ db/Creation.sql
@@ -4,35 +4,41 @@ full_name VARCHAR(100),
 username VARCHAR(100),
 password VARCHAR(100)
 
-)
+);
 
-create table order (
+create table cmd (
 
 id_order integer PRIMARY KEY,
-food VARCHAR(100),
+food VARCHAR(100) REFERENCES food,
 price decimal(10,2),
 quantity integer,
 total VARCHAR(100),
-date VARCHAR(100),
+order_date VARCHAR(100),
 customor_name Varchar(100),
 customor_contact Varchar(100),
 customor_mail Varchar(255),
 customor_address Varchar(255)
 
-)
+);
 
 create table category (
 id_categoty Serial PRIMARY Key,
 title Varchar(100),
-description Varchar(255)
-)
-
+description Varchar(255),
+image_name Varchar(255),
+featured VARCHAR(15),
+active Varchar(10)
+);
 
 create table food (
 id_food Serial PRIMARY Key,
 title Varchar(100),
+price decimal(10,2),
 description Varchar(255),
-id_categoty  Varchar (100),
-image_name VARCHAR(255)
+id_categoty  Varchar (100) REFERENCES category,
+image_name VARCHAR(255),
+featured VARCHAR(15),
+active Varchar(10)
+);
 
-)
+
