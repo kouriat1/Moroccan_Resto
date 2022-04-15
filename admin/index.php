5327291f3ae491cad380126232b0d81a94457586
Mohamed Kouriat <mohamed.kouriat.etu@univ-lille.fr>
2022-04-15 02:13:40 +0200

    Section

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,21 @@
         <title> Tajeat website</title>
     </head>
     <body>
-        <h1>Admin Panel</h1>    
+        <h1>Admin Panel</h1>
+        
+        
+        <!-- Menu Section Starts -->
+        <div class="menu">
+
+        Menu here
+        </div>
+
+        <!-- Menu Section  ends  -->
+        <!-- Footer Start -->
+        <div class="Footer">
+            Footer here
+        </div>
+        <!-- Footer end -->
+
     </body>
-</html>
+</html> 
